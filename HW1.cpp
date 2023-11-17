f94c503971d5f6b8d55ebe83eb033da385ba62f7
jiashengwu0717 <wujiasheng0717@gmail.com>
2023-11-16 20:08:31 -0500

    Complete Exercise 2

--- HW1.cpp
+++ HW1.cpp
@@ -17,5 +17,25 @@ int main()
 
     std::cout << "Get: " << list.Get(1) << "\n";
     std::cout << "Index of 11 (dne): " << list.IndexOf(11) << "\n";
+
+    std::cout << std::endl;
+
+    DoublyLinkedList<int> DList;
+    DList.Add(10);
+    DList.Add(20);
+    DList.Add(30);
+    DList.Insert(15,1);
+
+    std::cout << "Doubly List size: " << DList.Size() << std::endl;
+
+    // Using iterator to traverse the list
+    DListIterator<int> iterator = DList.Iterator();
+    while (iterator.HasNext()) {
+        std::cout << iterator.Next() << std::endl;
+    }
+
+    std::cout << "Get: " <<DList.Get(1) << "\n";
+    std::cout << "Index of 11 (dne): " << DList.IndexOf(11) << "\n";
+
     return 0;
 }
